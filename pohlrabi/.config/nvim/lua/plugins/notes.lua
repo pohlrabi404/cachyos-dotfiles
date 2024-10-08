7db8cdc8d8589905d8838f326fb4b20453442817
Pohlrabi <pohlrabi404@gmail.com>
2024-10-09 00:43:26 +0900

    change telekasten to scroll

--- pohlrabi/.config/nvim/lua/plugins/notes.lua
+++ pohlrabi/.config/nvim/lua/plugins/notes.lua
@@ -1,35 +1,5 @@
 return {
   {
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup({
-        auto_load = true,        -- whether to automatically load preview when
-        -- entering another markdown buffer
-        close_on_bdelete = true, -- close preview window on buffer delete
-
-        syntax = true,           -- enable syntax highlighting, affects performance
-
-        theme = 'dark',          -- 'dark' or 'light'
-
-        update_on_change = true,
-
-        app = 'qutebrowser',                           -- 'webview', 'browser', string or a table of strings
-
-        filetype = { 'markdown', 'md', 'telekasten' }, -- list of filetypes to recognize as markdown
-
-        -- relevant if update_on_change is true
-        throttle_at = 200000,   -- start throttling when file exceeds this
-        -- amount of bytes in size
-        throttle_time = 'auto', -- minimum amount of time in milliseconds
-        -- that has to pass before starting new render
-      })
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-  },
-  {
     'renerocksai/telekasten.nvim',
     dependencies = { 'nvim-telescope/telescope.nvim' },
     config = function()
@@ -49,7 +19,11 @@ return {
   {
     "oflisback/obsidian-bridge.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    config = function() require("obsidian-bridge").setup() end,
+    config = function()
+      require("obsidian-bridge").setup({
+        scroll_sync = true
+      })
+    end,
     event = {
       "BufReadPre *.md",
       "BufNewFile *.md",
